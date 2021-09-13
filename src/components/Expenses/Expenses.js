d1aa177374cb0144c4b949031d9fd1806293669e
rsachdeva1020 <rohansachdeva1020@gmail.com>
2021-09-13 22:54:05 +0530

    added Filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,17 @@
 import ExpenseItem from './ExpenseItem'
+import Filter from './Filter'
 
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    };
     return (
         <div>
+            <Filter selected={filteredYear} onChangeFilter={filterChangeHandler} />
             {props.items.map(expense => <ExpenseItem prop={expense}></ExpenseItem>)}            
         </div>
     )
---
+++ src/components/Expenses/Filter.js
@@ -0,0 +1,18 @@
+
+
+export default function Filter(props){
+    function dropdownChangeHandler(event){
+        props.onChangeFilter(event.target.value)
+    }
+    return(
+        <div>
+            <label>Filter by year</label>
+            <select value={props.selected} onChange={dropdownChangeHandler}>
+                <option value='2022'>2022</option>
+                <option value='2021'>2021</option>
+                <option value='2020'>2020</option>
+                <option value='2019'>2019</option>
+            </select>
+        </div>
+    )
+}
